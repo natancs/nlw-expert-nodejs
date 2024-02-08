6897e558c56925d10cb934c070902944847326ad
Natanael Carvalho <natan.carvalho123@hotmail.com>
2024-02-08 19:25:58 +0000

    create lib and routes folder

--- request.bash
+++ request.bash
@@ -1,3 +1,3 @@
 curl -X POST http://localhost:3333/polls \
    -H 'Content-Type: application/json' \
-   -d '{"title":"testando"}'
+   -d '{"title":"testando", "options":["Express","Fastify","NodeJS","HapiJS"]}'
