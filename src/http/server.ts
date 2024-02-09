dfb81518a58d64e91e016dc71497569e6667f739
Natanael Carvalho <natan.carvalho123@hotmail.com>
2024-02-09 15:25:30 +0000

    finishing project

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,11 @@
 import fastify from "fastify";
 import cookie from "@fastify/cookie"
+import websocket from "@fastify/websocket";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
+"@fastify/websocket";
 
 const app = fastify()
 
@@ -10,10 +13,12 @@ app.register(cookie, {
   secret: "pools-app-nlw",
   hook: "onRequest"
 })
+app.register(websocket)
 
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!")
